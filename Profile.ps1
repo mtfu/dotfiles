0d5411c1b9fe771505cf4708ee71a507e779c613
Michael Futtrup <mfu@sekoia.dk>
2021-04-22 09:48:01 +0200

    Updated script to handle hooksPath smoothly and added more programs to install

--- Profile.ps1
+++ Profile.ps1
@@ -22,3 +22,8 @@ function sr {
 function t($command) {
     wsl.exe dstask $command
 }
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
--- Sekoia/setup.ps1
+++ Sekoia/setup.ps1
@@ -1,5 +1,6 @@
 New-Item -ItemType SymbolicLink -Path $env:USERPROFILE\hooks -Target $env:userprofile\dotfiles\sekoia\hooks;
-git config --global core.hooksPath $env:USERPROFILE\hooks;
+$path = "$env:USERPROFILE\hooks".Replace('\', '\\')
+Add-Content ~\.gitconfig-local "`n[Core]`n  hooksPath = $path"
 
 #choco
 choco install microsoftazurestorageexplorer slack -y
