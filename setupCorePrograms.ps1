6e05b969cd46d46ea5e9ce8f7067896b70cbea77
Michael Futtrup <Epico_mtfu@ok.dk>
2023-02-05 00:41:59 +0100

    Install mingw also, to make sure we can compile all treesitter parsers (always good to have a backup compiler)

--- setupCorePrograms.ps1
+++ setupCorePrograms.ps1
@@ -1,1 +1,2 @@
-choco install git powershell-core oh-my-posh fzf kdiff3 brave autohotkey powertoys nodejs neovim 7zip vscode bat tidal ntop.portable fd ripgrep starship zig zoxide -y
+# zig and mingw are used for treesitter so it can compile all the parsers, somehow on my laptop mingw are nessary to compile svelte so I just install both
+choco install git powershell-core oh-my-posh fzf kdiff3 brave autohotkey powertoys nodejs neovim 7zip vscode bat tidal ntop.portable fd ripgrep starship zig mingw zoxide -y
