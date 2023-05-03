d682afef562c02e4eaea3209fc4fa6deb24350b6
Michael Futtrup <Epico_mtfu@ok.dk>
2023-05-03 16:39:13 +0200

    Do not drop selection when tabbed multiple lines

--- .config/nvim/lua/mtfu/maps.lua
+++ .config/nvim/lua/mtfu/maps.lua
@@ -6,6 +6,9 @@ vim.keymap.set('n', 'dw', 'vb"_d')
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set('v', '<', '<gv', {noremap=true, silent=true})
+vim.keymap.set('v', '>', '>gv', {noremap=true, silent=true})
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
