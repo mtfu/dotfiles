3fcbfe00a32e7182f4f5611506b5b900ec46dfe8
Michael Futtrup <Epico_mtfu@ok.dk>
2025-06-27 22:57:40 +0200

    removed programs from setupCorePrograms.ps1

--- setupCorePrograms.ps1
+++ setupCorePrograms.ps1
@@ -1,1 +1,1 @@
-choco install git powershell-core oh-my-posh fzf kdiff3 brave autohotkey powertoys nodejs neovim bat ntop.portable fd ripgrep starship zig zoxide lazygit sshs nvm -y
+choco install git powershell-core oh-my-posh fzf kdiff3 powertoys neovim bat ntop.portable fd ripgrep starship zig zoxide lazygit sshs nvm -y
